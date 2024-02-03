f05ae6b1c49243b5d344a70da08ca328e71a825d
awareermine <aramie.ewen@gmail.com>
2024-02-03 03:08:38 -0500

    cards with facts and guesses

--- client/src/components/factCard.tsx
+++ client/src/components/factCard.tsx
@@ -1,23 +1,25 @@
 interface FactCardProps {
-    facts: string[],
-    guesses: string[],
+  facts: string[];
+  guesses: string[];
 }
 
-const FactCard: React.FC<FactCardProps> = ({facts, guesses}) => {
-    return (
-        <div className="flex flex-col gap-2 bg-gray-300 bg-opacity-60 rounded-md">
-            <div className="">
-                {facts.map(fact => (
-                    <div>{fact}</div>
-                ))}
-            </div>
-            <div>
-                {guesses.map(guess => (
-                    <div>{guess}</div>
-                ))}
-            </div>
-        </div>
-    )
-}
+const FactCard: React.FC<FactCardProps> = ({ facts, guesses }) => {
+  return (
+    <div className="flex flex-col gap-2 p-2 bg-neutral-300 rounded-md">
+      <ol className="flex-grow list-decimal list-inside">
+        {facts.map((fact) => (
+          <li key={fact}>{fact}</li>
+        ))}
+      </ol>
+      <div className="flex gap-1 flex-wrap">
+        {guesses.map((guess) => (
+          <div key={guess} className="bg-amber-200 px-1 rounded-sm">
+            {guess}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default FactCard;
