8af7d302d88341fae63c9e32573558f44070ed59
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-02-03 12:53:27 -0500

    fetcher util

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,8 +1,27 @@
 import { createRoute } from "@tanstack/react-router";
 import { rootRoute } from "./__root";
+import { request } from "../lib/fetcher";
+import { z } from "zod";
 
 export const indexRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/",
-  component: () => <div>Hi there</div>,
+  loader: async () => {
+    return await request({
+      route: "/",
+      schema: z.object({
+        message: z.string(),
+      }),
+    });
+  },
+  component: () => {
+    const data = indexRoute.useLoaderData();
+
+    return (
+      <div>
+        <h1>Index Route</h1>
+        <p>{data.message}</p>
+      </div>
+    );
+  },
 });
