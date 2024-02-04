9a7409e55f01cc85cfd3f4b86e8e5bcf9bacf9d9
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-02-04 00:47:51 -0500

    answering state view maybe might be ready for backend integration

--- client/src/components/room-play-view.tsx
+++ client/src/components/room-play-view.tsx
@@ -5,38 +5,48 @@ import { z } from "zod";
 import { Textarea } from "./ui/textarea";
 import { useGameStore } from "@/lib/game";
 import { useEffect, useState } from "react";
+import { ChevronRightIcon } from "@heroicons/react/16/solid";
 
-const useTimerLoop = () => {
-  const duration = 30;
+const useTimerLoop = (duration: number, callback: () => void) => {
   const [startTime] = useState(new Date());
-  const [timeRemaining, setTimeRemaining] = useState(0);
+  const [timeRemaining, setTimeRemaining] = useGameStore((s) => [
+    s.timeLeft,
+    s.setTimeLeft,
+  ]);
 
   useEffect(() => {
     const interval = setInterval(() => {
       const now = new Date();
       const elapsed = now.getTime() - startTime.getTime();
-      const remaining = duration - elapsed / 1000;
+      const remaining = duration + 1 - elapsed / 1000;
 
       if (remaining < 0) {
         clearInterval(interval);
+
         setTimeRemaining(0);
+
+        callback();
       } else {
         setTimeRemaining(Math.ceil(remaining));
       }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [duration, startTime, timeRemaining]);
+  }, [duration, startTime, timeRemaining, setTimeRemaining, callback]);
 
   return timeRemaining;
 };
 
-export const AnsweringStateView = () => {
-  const question = "How's it hanging?";
+const responseFormSchema = z.object({
+  response: z.string().min(5).max(300),
+});
 
-  const responseFormSchema = z.object({
-    response: z.string().min(5).max(300),
-  });
+export const AnsweringStateView = () => {
+  const setStateMatching = useGameStore((s) => s.setStateMatching);
+
+  useTimerLoop(60, setStateMatching);
+
+  const question = "How's it hanging?";
 
   const form = useZodForm({
     schema: responseFormSchema,
@@ -50,29 +60,32 @@ export const AnsweringStateView = () => {
   };
 
   return (
-    <main className="h-screen flex items-center">
+    <div className="flex items-center h-[50vh]">
       <div className="flex flex-col items-center mx-auto w-full h-max">
-        <h1 className="font-whosit text-5xl">timer</h1>
-        <div className="flex flex-col items-center gap-4 justify-center w-[95%] md:w-1/2 bg-orange-500/25 rounded-3xl px-4 md:px-8 py-5 border-4 border-stone-800 shadow-rose-700 shadow-md">
-          <h1 className="text-2xl font-bold">{question}</h1>
+        <div className="flex flex-col items-center gap-4 justify-center w-[90%] md:w-3/4 lg:w-1/2 bg-orange-500/25 rounded-3xl px-4 md:px-8 py-5 border-4 border-stone-800 shadow-rose-700 shadow-md">
+          <p className="text-2xl font-medium">{question}</p>
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(onSubmit)}
-              className="flex items-center w-full lg:w-[70%] xl:w-[60%]"
+              className="flex items-center w-full lg:w-3/4 my-4"
             >
               <FormField
                 control={form.control}
                 name="response"
                 render={({ field }) => (
                   <FormItem className="w-full">
-                    <FormControl>
-                      <div className="flex flex-col gap-2 items-center">
+                    <FormControl className="w-full">
+                      <div className="flex flex-col gap-2 items-center w-full">
                         <Textarea
                           {...field}
+                          className="w-full"
                           placeholder="Thoughtful response..."
                         />
-                        <Button type="submit" className="font-bold">
+                        <Button type="submit" className="font-bold w-full">
                           Go!
+                          <span className="h-4 w-4">
+                            <ChevronRightIcon />
+                          </span>
                         </Button>
                       </div>
                     </FormControl>
@@ -84,36 +97,41 @@ export const AnsweringStateView = () => {
           </Form>
         </div>
       </div>
-    </main>
+    </div>
   );
 };
 const MatchingStateView = () => {
+  const setStateAnswering = useGameStore((s) => s.setStateAnswering);
+  useTimerLoop(60, setStateAnswering);
+
   return <div>hdwai</div>;
 };
 
 const GameState = () => {
   const gameState = useGameStore((s) => s.state);
 
-  if (gameState === "answering") {
-    return <AnsweringStateView />;
-  }
+  return <AnsweringStateView />;
 
-  if (gameState === "matching") {
-    return <MatchingStateView />;
-  }
+  // if (gameState === "answering") {
+  //   return <AnsweringStateView />;
+  // }
 
-  return <div></div>;
+  // if (gameState === "matching") {
+  //   return <MatchingStateView />;
+  // }
+
+  // return <div></div>;
 };
 
 export const RoomPlayView = () => {
-  const timeRemaining = useTimerLoop();
+  const timeLeft = useGameStore((s) => s.timeLeft);
 
   return (
     <main className="max-w-[100rem] py-6 px-4 md:mx-auto md:w-[70%]">
-      <nav className="flex justify-between items-center gap-8 w-full mx-auto md:w-1/2 bg-orange-500/25 rounded-3xl h-max p-6 border-4 border-stone-800 shadow-rose-700 shadow-md">
+      <nav className="flex justify-between items-center gap-8 w-full mx-auto md:w-3/4 lg:w-1/2 bg-orange-500/25 rounded-3xl h-max p-6 border-4 border-stone-800 shadow-rose-700 shadow-md">
         <p className="font-semibold text-xls">Nirjhor Nath</p>
         <p className="text-3xl bg-pink-600 p-3 font-bold rounded-full h-12 w-12 flex items-center justify-center border-2 border-stone-800">
-          {timeRemaining}
+          {timeLeft}
         </p>
         <p className="font-mono text-lg text-emerald-950 bg-emerald-300 px-4 rounded-full border-2 border-stone-800">
           Points: 38
--- client/src/lib/game.ts
+++ client/src/lib/game.ts
@@ -2,10 +2,17 @@ import { create } from "zustand";
 
 type GameStore = {
   state: "answering" | "matching" | "finished";
+  timeLeft: number;
+  setTimeLeft: (time: number) => void;
+  setStateAnswering: () => void;
+  setStateMatching: () => void;
+  setStateFinished: () => void;
 };
 
 export const useGameStore = create<GameStore>((set) => ({
   state: "matching",
+  timeLeft: 0,
+  setTimeLeft: (time: number) => set({ timeLeft: time }),
   setStateAnswering: () => set({ state: "answering" }),
   setStateMatching: () => set({ state: "matching" }),
   setStateFinished: () => set({ state: "finished" }),
