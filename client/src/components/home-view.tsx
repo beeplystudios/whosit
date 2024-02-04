ca967a1003b7d877dbf77197bd455bfc8b314fca
Nirjhor Nath <nirjhor.nath@gmail.com>
2024-02-04 12:01:55 -0500

    cleanup

--- client/src/components/home-view.tsx
+++ client/src/components/home-view.tsx
@@ -166,35 +166,6 @@ const JoinRoom = () => {
 };
 
 export const HomeView = () => {
-  // const data = indexRoute.useLoaderData();
-
-  // const factses = [
-  //   [
-  //     "I ate a slug as a kid, it was on purpose",
-  //     "I still do it sometimes, you know",
-  //     "nobody ever understands",
-  //     "I'm slug man",
-  //   ],
-  //   [
-  //     "I didn't eat anything particularly weird as a kid, besides a worm",
-  //     "I still do it sometimes, you know",
-  //     "nobody ever understands",
-  //     "I'm worm man",
-  //   ],
-  //   [
-  //     "I ate a your mom's ass as a kid, it was on purpose",
-  //     "I still do it sometimes, you know",
-  //     "nobody ever understands",
-  //     "I'm you're mom's as's man",
-  //   ],
-  // ];
-
-  // const guesseses = [
-  //   ["slugman", "slugman", "lucy"],
-  //   ["wormman", "wormman", "janet"],
-  //   ["you'remom'sass'sman", "you'remom'sass'sman", "eleanor"],
-  // ];
-
   const [parent] = useAutoAnimate();
   const params = indexRoute.useSearch();
 
